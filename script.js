bbc0e14741ffa4f730a26375c8723ad6511bf6e1
FloppaYT1337 <zuevroman179@gmail.com>
2025-02-17 15:54:30 +0800

    test
    test

---
+++ script.js
@@ -0,0 +1,13 @@
+var bottom = 21;
+var left = 20;
+
+if (bottom > left) print('Bottom is bigger than left! Reducing');
+wait(3);
+bottom = bottom - 2;
+
+if (bottom < left) print('well done');
+end;
+
+if (bottom = left)
+ print("everything seems normal")
+end;
